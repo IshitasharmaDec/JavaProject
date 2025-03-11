d6565e7b0f1315c7ac2509f9cd80fb531d8b5829
IshitasharmaDec <sharmaishita3112@gmail.com>
2025-03-11 13:29:16 +0530

    10th March 2025 pushing to git

---
+++ src/JavaBasic/Printf.java
@@ -0,0 +1,29 @@
+package JavaBasic;
+
+public class Printf {
+    public static void main(String[] args) {
+        int n =9, i=1;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+        i++;
+        System.out.printf("%d*%d=%d\n",n,i,(n*i));
+
+
+
+    }
+}
